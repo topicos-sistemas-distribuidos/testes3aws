8f9f50f1575a2fbf5d3d93a9dba737b791e69f34
armandossrecife <armando.sousa@gmail.com>
2019-03-21 19:51:28 -0300

    Foi feito um refactory para tratar exceções genéricas na chamada principal da classe App de teste do serviço.

--- src/main/java/br/ufc/great/es/tsd/s3/teste/App.java
+++ src/main/java/br/ufc/great/es/tsd/s3/teste/App.java
@@ -7,22 +7,32 @@ import br.ufc.great.es.tsd.s3.teste.util.Constantes;
 
 public class App {
     public static void main( String[] args ){
-    	String bucketName = "systagram-uploads";
-    	S3ClientManipulator s3Client = new S3ClientManipulator(bucketName);
-        bucketName = "systagram-uploads";
-
-        if (!s3Client.checkIfBucketExists()) {
-        	System.out.println("O bucket " + bucketName + " não existe!");
-        	return;
-        }
+    	String bucketName; 
+    	S3ClientManipulator s3Client; 
     	
-        String file_path = new Constantes().picturesDirectory + FileSystems.getDefault().getSeparator() + "2-2019-03-18-13-56-39.png";
-        String key_name = Paths.get(file_path).getFileName().toString();
+    	try {
+    		bucketName = "systagram-uploads";
+    		s3Client = new S3ClientManipulator(bucketName);
 
-        //faz upload de arquivo
-        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucketName);
-        if (s3Client.uploadFile(file_path)) {
-            System.out.println("Upload do arquivo " + key_name + " concluido com sucesso!");
-        }
+    		System.out.println("Acessando o S3 da AWS");
+    		
+    		if (!s3Client.checkIfBucketExists()) {
+    			System.out.println("O bucket " + bucketName + " não existe!");
+    			return;
+    		}else {
+    			System.out.println("O bucket " + bucketName + " foi encontrado");
+    		}
+
+    		String file_path = new Constantes().picturesDirectory + FileSystems.getDefault().getSeparator() + "2-2019-03-18-13-56-06.png";
+    		String key_name = Paths.get(file_path).getFileName().toString();
+
+    		//faz upload de arquivo
+    		System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucketName);
+    		if (s3Client.uploadFile(file_path)) {
+    			System.out.println("Upload do arquivo " + key_name + " concluido com sucesso!");
+    		}
+    	}catch (Exception e) {
+    		System.err.println("Erro: " + e.getMessage());
+    	}
     }
 }
